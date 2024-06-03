34d209358f7b18fe086a6b94c90063130622add5
feng <1034013213@qq.com>
2024-06-04 07:04:42 +0800

    fix modal action

--- src/components/UserAuthFormModel.jsx
+++ src/components/UserAuthFormModel.jsx
@@ -28,6 +28,26 @@ function UserAuthFormModel() {
                 onOpenChange={onOpenChange}
                 placement="center"
                 className={"overflow-hidden"}
+                motionProps={{
+                    variants: {
+                        enter: {
+                            y: 0,
+                            opacity: 1,
+                            transition: {
+                                duration: 0.3,
+                                ease: "easeOut",
+                            },
+                        },
+                        exit: {
+                            y: -20,
+                            opacity: 0,
+                            transition: {
+                                duration: 0.2,
+                                ease: "easeIn",
+                            },
+                        },
+                    }
+                }}
             >
                 <ModalContent className={"flex items-center justify-center "}>
                     {(onClose) => (
